cb36e5160e667c610f048994011ac35402c62d86
antyj1994 <antyj1994@gmail.com>
2018-01-14 17:29:18 +0100

    added logo

--- Improvegg/WebContent/js/servizi.js
+++
@@ -1,9 +0,0 @@
-function Partita(gameMode, champName, kda){
-	this.gameMode = gameMode;
-	this.champName = champName;
-	this.kda = kda;
-	
-	this.info = function(){
-		return this.gameMode + ", " + this.champName + ", " + this.kda;
-	}
-}
