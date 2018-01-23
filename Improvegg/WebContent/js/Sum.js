3841fb006c423b04993b71c814408d9bb32d2301
antyj1994 <antyj1994@gmail.com>
2018-01-24 00:18:31 +0100

    ready to test

--- Improvegg/WebContent/js/Sum.js
+++ Improvegg/WebContent/js/Sum.js
@@ -19,4 +19,55 @@ $(document).ready(function() {
 		$(this).closest('.dropup').removeClass('dropup');
 		$(this).closest('.jq-match-container').find('.jq-dropdown-menu').hide();
 	});*/
+	var xhr = new XMLHttpRequest();
+	xhr.setRequestHeader("connection", "close");
+	xhr.open('get', "expandSummoner", true);
+	xhr.onload = function() {
+		
+		var jsonStringQuotes = xhr.responseText;
+		var dati = JSON.parse(jsonStringQuotes);
+
+		/*var xhrA = new XMLHttpRequest();
+		xhrA.open('get', 'datiAnagrafici');
+		xhrA.onload = function() {
+			var jsonStringQuotesA = xhrA.responseText;
+			var datiAnagrafici = JSON.parse(jsonStringQuotesA);
+			var jsonStringQuotes = xhr.responseText;
+			var professori = JSON.parse(jsonStringQuotes);
+			var v = $('<option value=\"-1\">---</option>');
+			$("#opzioniRicevimenti").append(v);
+			var cont=0;
+			for ( var i in professori) {
+				console.log(professori[i].studentiRicevimento);
+				if(professori[i].nomeUtente == datiAnagrafici.nomeUtente){
+					for( var j in professori[i].studentiRicevimento){
+						var a = $('<tr> <th value=\"'+professori[i].studentiRicevimento[j].matricola+'\">'+professori[i].studentiRicevimento[j].nome+' '+professori[i].studentiRicevimento[j].cognome+'</th> <th id="data'+cont+'"></th>  </tr>');
+						$("#listaRicevimenti").append(a);
+						texpandMatch(professori[i].nomeUtente, professori[i].studentiRicevimento[j].matricola,cont);
+						cont++;
+						var d = $('<option value=\"'+professori[i].studentiRicevimento[j].matricola+'\">'+professori[i].studentiRicevimento[j].nome+' '+professori[i].studentiRicevimento[j].cognome+'</option>');
+						$("#opzioniRicevimenti").append(d);
+					
+					}
+				}
+			}	
+		}*/
+	}
+	xhr.send(null);
 });
+
+function expandMatch(nM,mt,cont) {
+	
+	$.ajax({
+		url : 'expandSummoner',
+		data : {
+			nomeUtente : nM,
+			matricola : mt
+		},
+		type : 'get',
+		success: function(data){
+			$('#data'+cont+'').text(data);	
+		}
+	});
+	
+}
--- Improvegg/src/controller/expandSummoner.java
+++ Improvegg/src/controller/expandSummoner.java
@@ -1,5 +1,30 @@
 package controller;
 
-public class expandSummoner {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+public class expandSummoner extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		String matricola = (String) session.getAttribute("matricola");
+		String nomeUtente = req.getParameter("nomeUtente");
+		
+		resp.setContentType("application/json");
+		PrintWriter out=resp.getWriter();
+		Gson gson=new Gson();
+		out.print(gson.toJson(data));
+		out.close();
+		
+	}
 }
